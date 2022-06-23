1be2165799e33650c1fa0c2f2740fb42922d935a
kolabsgit <107631423+kolabsgit@users.noreply.github.com>
2022-06-23 16:42:00 +0200

    Add files

---
+++ build/lib/aircallConfig.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AIRCALL_AUTH_TOKEN = exports.API_TOKEN = exports.API_ID = void 0;
+exports.API_ID = "ac494eb42ab77c80278cdb31845bdbac";
+exports.API_TOKEN = "f1b942dad7a83ea397055c5924c46837";
+exports.AIRCALL_AUTH_TOKEN = "Basic YWM0OTRlYjQyYWI3N2M4MDI3OGNkYjMxODQ1YmRiYWM6ZjFiOTQyZGFkN2E4M2VhMzk3MDU1YzU5MjRjNDY4Mzc=";
